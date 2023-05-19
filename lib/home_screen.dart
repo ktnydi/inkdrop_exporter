2ed92604b990ac3cde94c6e08cdb2f03f2ebecc1
Yudai Kitano <36891892+ktnydi@users.noreply.github.com>
2023-05-19 17:57:02 +0900

    feat: アップロード画面を実装

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,48 +1,46 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          children: [
             const Text(
-              'You have pushed the button this many times:',
+              'InkdropのメモをMarkdownファイルに変換します。',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            const SizedBox(height: 16),
+            Expanded(
+              child: DottedBorder(
+                strokeWidth: 4,
+                dashPattern: const [8],
+                color: Theme.of(context).dividerColor,
+                radius: const Radius.circular(16),
+                borderType: BorderType.RRect,
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Text('ここにファイルをドラッグ&ドロップしてアップロード'),
+                      const SizedBox(height: 32),
+                      FilledButton(
+                        onPressed: () {
+                          // TODO: ファイルを取得する処理を実装する。
+                        },
+                        child: const Text('ファイルを選択'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
